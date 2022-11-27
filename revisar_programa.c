48208c6dbe74ad95bcc6124f59878da38520d489
William Elí O.L <ramun346416@gmail.com>
2022-11-26 23:31:54 -0600

    Se hace resta delimites
    Terminos=2
    a=2
    n=2
    limite superior x=2
    limite inferior y=1

--- revisar_programa.c
+++ revisar_programa.c
@@ -3,8 +3,9 @@
 void main()
 
 {
-float p, x, y, acumxxx, acumx1, acumx2, acumx3, acumy,acumxx, acumxx1, acumxx2 ,acumxx3 ,acumyy, acumyy1, acumyy2 ,acumyy3,acumy1, acumy2, acumy3;
+float p, x, y, acumxxx, acumx1, acumx2, acumx3, acumxx, acumxx1, acumxx2 ,acumxx3 ,acumyy, acumyy1, acumyy2 ,acumyy3,acumy1, acumy2, acumy3;
 int terminos, potencia ,i,j, b, c, d, m; //la variable d es la constante
+i=0;
 printf("\n Favor de intruducir las constantes de la ecuacion (a, b, c, d) y sus exponentes (n, m, p) \n");
 printf("\n aX^n+bX^m+cX^p+d \n\n");
 printf("\n [aX^n] + [bX^m] + [cX^p] + [d] \n");
@@ -13,16 +14,21 @@ printf("\n Proporcione la cantidad de terminos\n");
 scanf("%d",&terminos);
 float a[terminos];
 int n[terminos]; //solo potencias enteras, para potencias fraccionarias  se necesita sacar raices
-float acumx[terminos];
+float acumx[terminos], acumy[terminos];
+// este for es para limpiar las celdas de memoria del vector n[i]
+for (i=0;i<=terminos;i++)
+{
+n[i]=0;
+}
 //Se asigna valores a acomuladores, constante "a" y la potencia "n"
 for (i=0;i<terminos;i++)
 {
 printf("\n Favor de proporcionar la constante ""a"" del termino %d \n", i+1); // carga de exponentes y constantes
-printf("→→> [a] X^n");
+printf("→→> [a] X^n \n");
 scanf("%f",&a[i]);
 
 printf("\n Favor de proporcionar el exponente ""n"" del termino %d \n" , i+1); // Coloco  "i+1" porque si coloco "i" comenzara a aparecer desde "0"
-printf("aX^ [n] ←←←");
+printf("aX^ [n] ←←← \n");
 scanf("%d",&n[i]); //cargando datos al vector "n" por medio del contador "i"
 printf("hola %f a[i]\n",a[i]);
 }
@@ -33,33 +39,42 @@ printf("a [X] ^n \n");
 printf("   ↑\n");
 printf("   ↑\n");
 printf("   |\n");
-// printf(" ^\n");
-// printf(" ^\n");
 scanf("%f",&x);
-// float a[terminos];
-// float n[terminos];
-// float acumx[terminos];
-//int terminos, potencia ,i,j, b, c, d, m;
-//p=1;
-
-
-acumx1=0; //acomulador suma
+printf("\n Favor de proporcionar limite Inferior  \n");
+scanf("%f",&y);
+printf("a [X] ^n \n");
+printf("   ↑\n");
+printf("   ↑\n");
+printf("   |\n");
+// printf("   %f YYY\n",y);
+acumx1=0; //acomulador suma limite superior
+acumy1=0; //acomulador suma limite inferior
 //Este for cin variable i es de los terminos, funciona
 for (i=0;i<=terminos;i++)
 {
     acumx1=acumx1+acumx3;
     acumx2=1; //acomulador multiplicadora
     acumx3=0; //acomulador suma
+    acumy1=acumy1+acumy3;
+    acumy2=1; //acomulador multiplicadora
+    acumy3=0; //acomulador suma
     //Este for cin variable  es de los potencia, funciona
     for (j=0;j<n[i]+1;j++) // Al agregar n[i]+1 me eleva la potencia
 {
     acumx2=acumx2*x; //la variable "p" se esta uasando `para prueba
+    acumy2=acumy2*y; //la variable "p" se esta uasando `para prueba
+    // printf("hola %f YYYY\n", y);
 
 }
 acumx3=a[i]*((acumx2+acumx3)/(n[i]+1));
+acumy3=a[i]*((acumy2+acumy3)/(n[i]+1));
 //acumxx1=acumx3*a[i];
-    printf("hola %f\n", acumx3);
-    printf("hola %f a[i]\n",a[i]);
+    // printf("hola %f superior\n", acumx3);
+    // printf("hola %f a[i]\n",a[i]);
+    // printf("hola %f inferior\n", acumy3);
     }
-printf("Resultado de la Integral %f \n\n", acumx1);
+printf("Resultado de la Integral %f superior \n", acumx1);
+printf("Resultado de la Integral %f inferior \n\n", acumy1);
+acumxxx=acumx1-acumy1;
+printf("Resultado de la Integral %f resultado \n\n", acumxxx);
 }
